d2dc96df07e19fc059fd801181862ee657d3f058
Felipe Teles <felipersteles@gmail.com>
2023-04-17 10:41:48 -0300

    add: board package entities

---
+++ entities.py
@@ -0,0 +1,82 @@
+from exceptions import BoardException
+
+# Nivel de tabuleiro
+
+
+class Position:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+
+    def setValues(self, row, column):
+        self.row = row
+        self.column = column
+
+
+class Piece:
+    position = None
+
+    def __init__(self, board):
+        self.board = board
+
+    def possibleMoves():
+        return []
+
+    def possibleMove(self, position):
+        return self.possibleMoves()[position.row, position.column]
+
+    def isAPossibleMove(self):
+        matrix = self.possibleMoves()
+
+        for i in matrix:
+            for j in matrix[i]:
+                if matrix[i, j]:
+                    return True
+
+        return False
+
+
+class Board:
+    pieces = []
+
+    def __init__(self, rows, columns):
+        if (rows < 1 or columns < 1):
+            raise BoardException("Número de linhas ou colunas inválido.")
+
+    def getPieceFromPosition(self, position):
+        if (self.positionExists(position) == False):
+            raise BoardException("Posição não encontrada.")
+
+        return self.getPiece[position.row, position.column]
+
+    def placePiece(self, piece, position):
+        if (self.haveAPiece):
+            raise BoardException("Já existe uma peça nessa posição.")
+
+        self.pieces[position.row, position.column] = piece
+        piece.position = position
+
+    def removePiece(self, position):
+        if (self.positionExists(position) == False):
+            raise BoardException("Esta posição não existe no tabuleiro.")
+
+        if (self.getPieceFromPosition(position) == None):
+            return None
+
+        removedPiece = self.getPieceFromPosition(position)
+        removedPiece.position = None
+        self.pieces[position.row][position.column] = None
+
+        return removedPiece
+
+    def getPiece(self, row, column):
+        return self.pieces(row, column)
+
+    def positionExists(self, position):
+        return position.row >= 0 and position.row < self.rows and position.column >= 0 and position.column < self.columns
+
+    def haveAPiece(self, position):
+        if (self.positionExists(position) == False):
+            raise BoardException("Posição não encontrada.")
+
+        return self.getPieceFromPosition(position) != None
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from entities import *
+
+
 def __main__():
-    print("Hello world!")
+    Board(-1, -3)
 
 
 if (__name__ == '__main__'):
